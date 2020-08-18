fef763da324d1d6cae9c42540981394aded1d07f
Ryan <ryanliu967@gmail.com>
2020-08-18 03:44:02 -0700

    Add rotating boss turret

--- Assets/Scripts/Boss/BossTurretRotatePattern.cs
+++ Assets/Scripts/Boss/BossTurretRotatePattern.cs
@@ -6,16 +6,22 @@ public class BossTurretRotatePattern : Enemy
 {
     [SerializeField] int numPulses = 5;
     [SerializeField] float timeBetweenPulses = 0.3f;
-    [SerializeField] GameObject firingDirections;
+    [SerializeField] GameObject rotationEndpoints;
+    [SerializeField] float rotationSpeed = 1f;
     bool firingAttack = false;
-    
+    Transform aimPosition;
+    Transform targetPosition;
+    int targetPositionIndex = 0;
 
     private void Start() 
     {
         ResetShotCounter();
+        targetPosition = rotationEndpoints.transform.GetChild(targetPositionIndex);
+        aimPosition = transform.GetChild(0);
     }
     void Update()
     {
+        Aim();
         CountDownAndShoot();
     }
 
@@ -35,17 +41,32 @@ public class BossTurretRotatePattern : Enemy
         }
     }
 
+    private void Aim()
+    {
+        if (aimPosition.position == targetPosition.position)
+        {
+            if (targetPositionIndex == 0)
+            {
+                targetPositionIndex = 1;
+            }
+            else
+            {
+                targetPositionIndex = 0;
+            }
+            targetPosition = rotationEndpoints.transform.GetChild(targetPositionIndex);
+        }
+        aimPosition.position = Vector2.MoveTowards
+            (aimPosition.position, targetPosition.position, rotationSpeed * Time.deltaTime);
+    }
+
     IEnumerator FireAttackPattern()
     {
         for (int pulse = 0; pulse < numPulses; pulse++)
         {
-            foreach (Transform child in firingDirections.transform)
-            {
-                GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
-                Vector3 direction = child.transform.position - laser.transform.position;
-                Vector3 normalizedDirection = direction / direction.magnitude;
-                laser.GetComponent<Rigidbody2D>().velocity = normalizedDirection * projectileSpeed;
-            }
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            Vector3 direction = aimPosition.position - laser.transform.position;
+            Vector3 normalizedDirection = direction / direction.magnitude;
+            laser.GetComponent<Rigidbody2D>().velocity = normalizedDirection * projectileSpeed;
             yield return new WaitForSeconds(timeBetweenPulses);
         }
         ResetShotCounter();
